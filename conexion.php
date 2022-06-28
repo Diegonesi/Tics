0b368e86e9b6abbe4a3d4bb4da8c223645c1ecb2
Diegonesi <89754287+Diegonesi@users.noreply.github.com>
2022-06-27 21:19:38 -0400

    Create conexion.php
    Conexion a la base de datos en el servidor

---
+++ conexion.php
@@ -0,0 +1,7 @@
+<?php
+        $user = "root";
+        $pass = "";
+        $host = "localhost";
+        $db ="DB_TIC"; # nombre de la base de datos
+        $con=mysqli_connect($host,$user, $pass,$db);
+?>
